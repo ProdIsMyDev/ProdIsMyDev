b01f0059c973b28b04bd372452e4ff334784724c
CKO <chris@casualcode.org>
2020-12-31 02:31:15 +0100

    Have to write new tests
    Signed-off-by: CKO <chris@casualcode.org>

--- test/integration/widget_test.dart
+++ test/integration/widget_test.dart
@@ -3,43 +3,4 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:website/main.dart';
 
-void main() {
-  testWidgets('There is a Image Carousel to Display recent Projects',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-
-    expect(find.byKey(Key('landingSection_headline')), findsOneWidget);
-    expect(find.text("Developer & Streamer"), findsOneWidget);
-  });
-
-  testWidgets('There is a Sub Header, that describes what I do in my freetime',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-
-    expect(find.byKey(Key('landingSection_subHeader')), findsOneWidget);
-    expect(find.text("I like to work on cool projects and stream sometimes"),
-        findsOneWidget);
-  });
-
-  testWidgets('There is a Landing Section containing a Solid Color Background',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-
-    expect(find.byKey(Key('landingSection_background')), findsOneWidget);
-  });
-
-  testWidgets('There is a Section describing me', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-
-    expect(find.byKey(Key('introductionSection_header')), findsOneWidget);
-    expect(find.text("Hi, I'm Chris. Nice meeting you."), findsOneWidget);
-  });
-
-  testWidgets(
-      'There is a Introduction Section containing a Solid Color Background',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-
-    expect(find.byKey(Key('introductionSection_background')), findsOneWidget);
-  });
-}
+void main() {}
