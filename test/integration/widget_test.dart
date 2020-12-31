70ecd2992a6c786d634f2694d56da6252711bace
CKO <chris@casualcode.org>
2020-12-31 02:40:10 +0100

    Using internal Assets, to fix tests
    Signed-off-by: CKO <chris@casualcode.org>

--- test/integration/widget_test.dart
+++ test/integration/widget_test.dart
@@ -3,4 +3,18 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:website/main.dart';
 
-void main() {}
+void main() {
+  testWidgets('There is a Image Carousel to Display recent Projects',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+
+    expect(find.byKey(Key('project_carousel')), findsOneWidget);
+  });
+
+  testWidgets('There is a Landing Section containing a Solid Color Background',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+
+    expect(find.byKey(Key('landingSection_background')), findsOneWidget);
+  });
+}
