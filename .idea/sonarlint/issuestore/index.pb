1f2076f3560715803096df8d383fc82ba2b09f05
ProdIsMyDev <chris@casualcomapny.io>
2022-11-19 05:54:24 +0100

    Adapting new design & first page

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+R
+"src/images/logos/casualcompany.svg,e/c/ec30d0d2ba4998bf81997c31fb0d51c9a5b0f371
+L
+src/images/logos/oc_logo.svg,3/f/3fa66fe68c15e8a99d250026a8a43f2bd7f62816
+P
+ src/images/logos/Logo_OC_RGB.svg,5/d/5df49209e97542ef2054e82025d1d21d20278c63
---
+++ src/components/SimpleLayout.jsx
@@ -0,0 +1,20 @@
+import {Container} from '@/components/Container'
+
+export function SimpleLayout({title, subtitle, intro, children}) {
+    return (
+        <Container className="mt-16 sm:mt-32">
+            <header className="max-w-2xl">
+                <h2 className="text-4xl font-semi-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+                    {title}
+                </h2>
+                <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+                    {subtitle}
+                </h1>
+                <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+                    {intro}
+                </p>
+            </header>
+            <div className="mt-16 sm:mt-20">{children}</div>
+        </Container>
+    )
+}
